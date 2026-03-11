610067e06486e618bdc84ee5c2760d9b1c1dd7dd
xCeeJayx <christianjesus.sanandres25@gmail.com>
2026-03-11 16:57:47 +0800

    Fix PSR-4 class name in EmployeeController

--- app/Http/Controllers/Employee/EmployeeController.php
+++ app/Http/Controllers/Employee/EmployeeController.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Collection;
 use App\Models\{LeaveApplication, ApprovalStep};
 
-class LeaveController extends Controller
+class EmployeeController extends Controller
 {
     public function index(Request $request)
     {
